463875ec70771b88ee2fd1bcbb82e3ea172bf0ca
Megh Joshi <71820617+MJO7@users.noreply.github.com>
2026-08-15 02:54:58 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        #0 -- circular
+        #1 -- square
+        # num_stud_one = 0
+        # num_stud_zero = 0
+       
+        # for i in students:
+        #     if i==1:
+        #         num_stud_one+=1
+        #     else:
+        #         num_stud_zero+=1
+        # for j in sandwiches:
+        #     if j==1:
+        #         if num_stud_one==0:
+        #             break
+        #         num_stud_one-=1
+              
+                
+        #     elif j==0:
+        #         if num_stud_zero==0:
+        #             break
+        #         num_stud_zero-=1
+                
+
+        # return num_stud_one+num_stud_zero
+
+        #counter solution
+
+        res = len(students)
+        count = Counter(students)
+
+        for j in sandwiches:
+            if count[j]>0:
+                res-=1
+                count[j]-=1
+            else:
+                return res
+        return res
+    
